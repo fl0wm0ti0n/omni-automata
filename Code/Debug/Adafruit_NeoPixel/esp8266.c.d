213984777f36a09ece53188d90df27119cb4bdd2
Florian Gabriel <flow@p0cki.net>
2021-07-09 17:21:49 +0200

    gitignore hinzugefügt

--- Code/Debug/Adafruit_NeoPixel/esp8266.c.d
+++
@@ -1,2 +0,0 @@
-C:\Users\flow\AppData\Local\Temp\VMBuilds\home_arduino\nano_atmega328\Debug\Adafruit_NeoPixel\esp8266.c.o: \
- X:\DataHub\Projektmappen\Arduino\libraries\Adafruit_NeoPixel\esp8266.c
--- Code/Debug/home_constants.cpp.d
+++
@@ -1,3 +0,0 @@
-C:\Users\flow\AppData\Local\Temp\VMBuilds\home_arduino\nano_atmega328\Debug\home_constants.cpp.o: \
- C:\Users\flow\AppData\Local\Temp\VMBuilds\home_arduino\nano_atmega328\Debug\home_constants.cpp \
- C:\Users\flow\AppData\Local\Temp\VMBuilds\home_arduino\nano_atmega328\Debug\home_constants.h
