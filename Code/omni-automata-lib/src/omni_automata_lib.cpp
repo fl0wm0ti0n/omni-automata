b76e7b59ce49ecca7d12a72709499c7d2a0f5104
Florian Gabriel <flow@p0cki.net>
2023-01-20 14:10:04 +0100

    letztstand save

---
+++ Code/omni-automata-lib/src/omni_automata_lib.cpp
@@ -0,0 +1,10 @@
+/*
+ Name:		omni_automata_lib.cpp
+ Created:	3/7/2022 9:37:01 PM
+ Author:	flow
+ Editor:	http://www.visualmicro.com
+*/
+
+#include "omni_automata_lib.h"
+
+
---
+++ Code/omni-automata-lib/src/omni_automata_lib.h
@@ -0,0 +1,19 @@
+/*
+ Name:		omni_automata_lib.h
+ Created:	3/7/2022 9:37:01 PM
+ Author:	flow
+ Editor:	http://www.visualmicro.com
+*/
+
+#ifndef _omni_automata_lib_h
+#define _omni_automata_lib_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+
+#endif
+
